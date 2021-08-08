556d8ede6d107dbea63c6c8cf1a801dbc769349c
malwaru <malikawarunamal@gmail.com>
2021-08-08 21:27:32 +0000

    Fixed the file path issue in importing world files

--- ros/src/pybullet_ros/plugins/environment_test_world.py
+++ ros/src/pybullet_ros/plugins/environment_test_world.py
@@ -2,10 +2,32 @@
 
 from pybullet_ros.plugins.environment import Environment as DefaultEnv
 import rospy
+import rospkg
 
 class Environment(DefaultEnv):
     def __init__(self, pybullet, **kargs):
         super().__init__(pybullet)
+
+    # def file_scan(self,world_name):
+    #     parent_found=False
+    #     counter=0
+    #     current_directory =os.path.abspath(os.getcwd())
+    #     while not parent_found and counter<10:    
+    #         current_directory=os.path.abspath(os.path.join(current_directory,os.pardir))    
+    #         foldername = os.path.basename(current_directory)
+
+    #         if  foldername=='ros':
+    #             parent_found=True   
+    #             current_directory=os.path.abspath(os.path.join(current_directory,os.pardir))         
+
+    #         counter+=1
+
+        
+
+    #     # world_file_name='test_world_v6.sdf'
+    #     # relative_path=world_file_name
+    #     world_folder=current_directory+'/common/test/sdf/worlds/'+world_name
+    #     return world_folder
 
     # override parent method
     def load_environment_via_code(self):
@@ -19,5 +41,17 @@ class Environment(DefaultEnv):
         self.pb.setPhysicsEngineParameter(sparseSdfVoxelSize=0.25) # ? related to soft bodies
         etc...
         """
-        self.pb.loadSDF('/home/ros_user/catkin_ws/src/pybullet_ros/common/test/sdf/worlds/test_world_v6.sdf')
-        rospy.logwarn('loading Gazebo world via code!')
+       ## Name of the world file you want to use 
+        world_file_name='test_world_v6.sdf'
+
+
+        ## inding the path to the world
+        rospack = rospkg.RosPack()
+        rospack_path=rospack.get_path('pybullet_ros')
+        rospack_path=rospack_path+'/common/test/sdf/worlds/'+world_file_name       
+        rospy.loginfo('Loading world file  : ' + rospack_path )         
+
+
+        self.pb.loadSDF(rospack_path)
+
+        rospy.logwarn('loading Pybullet world via code!')
